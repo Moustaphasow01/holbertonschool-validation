fa18f8612eba9ddbbf9ddbc4632b5564457b96e6
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2023-01-25 00:59:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ apt install curl -y
 curl --silent --location https://deb.nodesource.com/setup_16.x | bash -
 apt install nodejs -y
 npm install -g markdownlint-cli 1>&2
-markdownlint module4_task1/README.md
-markdownlint module4_task1/DEPLOY.md
+markdownlint module1_task3/README.md
+markdownlint module1_task3/DEPLOY.md
